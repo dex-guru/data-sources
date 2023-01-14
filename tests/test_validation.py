518ee6b06fff9cb8cf891f748d7c7138837535c4
Vitaly Vakhteev <vitaly.v@techusage.com>
2023-01-14 11:06:24 -0800

    start rewrite

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -4,27 +4,34 @@ from pathlib import Path
 import pytest
 from jsonschema import validate
 
-SCHEMA_NAME = 'tokenlist.schema.json'
+SCHEMA_NAME_CHAINS = 'chains.schema.json'
+SCHEMA_NAME_DAPPS = 'dapps.schema.json'
 
 
 @pytest.fixture()
-def get_schema():
-    with open(f'tests/{SCHEMA_NAME}') as f:
+def get_schema_chains():
+    with open(f'tests/{SCHEMA_NAME_CHAINS}') as f:
+        return json.load(f)
+
+@pytest.fixture()
+def get_schema_dapps():
+    with open(f'tests/{SCHEMA_NAME_DAPPS}') as f:
         return json.load(f)
 
 
 @pytest.mark.parametrize('file', Path(Path().root).glob('*.json'))
-def test_token_list_schema(file, get_schema):
+def test_chains_schema(file, get_schema):
     with open(file) as f:
-        token_list = json.load(f)
-        validate(token_list, get_schema)
+        cd_list = json.load(f)
+        validate(cd_list, get_schema)
 
 
 @pytest.mark.parametrize('file', Path(Path().root).glob('*.json'))
 def test_token_tag_in_tags_list(file):
     """Check that all tokens in the list have a tag in the tags list."""
     with open(file) as f:
-        token_list = json.load(f)
-        for token in token_list['tokens']:
+        cd_list = json.load(f)
+        for token in cd_list['tokens']:
             for tag in token['tags']:
-                assert tag in token_list['tags']
+                assert tag in cd_list['tags']
+# TODO: Rewrite
